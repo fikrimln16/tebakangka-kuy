b0dca327289e28c988580c8436ced55f64e5f175
Fikri Maulana <maulanafikri455@gmail.com>
2023-05-07 19:52:43 +0700

    UPDATED 5/7/2023 : add hint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import './App.css'
+import "./App.css";
 
 function generateRandomNumbers(length) {
   let numbers = new Set();
@@ -10,7 +10,6 @@ function generateRandomNumbers(length) {
   return Array.from(numbers);
 }
 
-
 const App = () => {
   const [chance, setChance] = useState(10);
   const [array, setArray] = useState(generateRandomNumbers(6));
@@ -18,16 +17,17 @@ const App = () => {
   const [openingMessages, setOpeningMessages] = useState([
     "Selamat datang di permainan tebak angka!",
     "Ketik 'exit' kapan saja untuk keluar dari permainan.",
-    "Silakan masukkan tebakan Anda:"
+    "Silakan masukkan tebakan Anda:",
   ]);
   const [messages, setMessages] = useState([]);
+  const [succes, setSuccess] = useState([]);
   const [guesses, setGuesses] = useState(["", "", "", "", "", ""]);
 
   useEffect(() => {
     setOpeningMessages([
       "Selamat datang di permainan tebak angka!",
       "Ketik 'exit' kapan saja untuk keluar dari permainan.",
-      "Silakan masukkan tebakan Anda:"
+      "Silakan masukkan tebakan Anda:",
     ]);
   }, []);
 
@@ -46,24 +46,35 @@ const App = () => {
       let inputNumbers = input.split(" ");
       let correctNumbers = 0;
       let correctPositions = 0;
+      let posisiBenar = [];
       for (let i = 0; i < array.length; i++) {
         if (inputNumbers.includes(array[i])) {
           correctNumbers++;
         }
         if (inputNumbers[i] === array[i]) {
           correctPositions++;
+          posisiBenar.push(inputNumbers[i]);
         }
       }
+      if(posisiBenar.length > 0 ){
+        setSuccess([...succes, `Asik, Angka ${posisiBenar} sudah benar posisinya!`])
+      }
       if (correctPositions === 6) {
-        setMessages([...messages, "Selamat, Anda berhasil menebak angka. Refresh, untuk memulai kembali!"]);
-        setGameOver(true);
+        setMessages([
+          ...messages,
+          "Selamat, Anda berhasil menebak angka. Refresh, untuk memulai kembali!",
+        ]);
+        setGameOver(true)
       } else {
         setChance(chance - 1);
         if (chance === 0) {
           setMessages([`Anda gagal, jawaban yang benar adalah: ${array}`]);
           setGameOver(true);
         } else {
-          setMessages([...messages, `Angka anda adalah : ${inputNumbers}. Angka yang benar: ${correctNumbers} dan Posisi yang benar: ${correctPositions}, tersisa ${chance} kesempatan.`]);
+          setMessages([
+            ...messages,
+            `Angka anda adalah : ${inputNumbers}. Angka yang benar: ${correctNumbers} dan Posisi yang benar: ${correctPositions}, tersisa ${chance} kesempatan.`,
+          ]);
           setGuesses(["", "", "", "", "", ""]);
         }
       }
@@ -79,7 +90,18 @@ const App = () => {
       </div>
       <div>
         {guesses.map((guess, index) => (
-          <input key={index} type="text" value={guess} onChange={(event) => handleInput(index, event.target.value)} />
+          <input
+            key={index}
+            type="number"
+            value={guess}
+            onChange={(event) => handleInput(index, event.target.value)}
+            required
+          />
+        ))}
+      </div>
+      <div>
+      {succes.map((message, index) => (
+          <p key={index}>{message}</p>
         ))}
       </div>
       <button onClick={handleGuess} disabled={gameOver}>
@@ -90,11 +112,9 @@ const App = () => {
           <p key={index}>{message}</p>
         ))}
       </div>
-      <div>
-        created by pikeriii.
-      </div>
+      <div>created by pikeriii.</div>
     </div>
   );
-}
+};
 
 export default App;
